3082875b80c0ad01cf0648500599a80615e58a67
smeubank <sme0001@auburn.edu>
2019-03-22 22:47:04 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var twilio = require('twilio');
+var app = express();
+var xml = require('xml');
+var o2x = require('object-to-xml');
+
+
+// This responds with TwiML
+app.get('/boknows', function (req, res) {
+    console.log("Got a GET request for Twiml");
+    res.set('Content-Type', 'text/xml');
+    res.send(o2x({
+        '?xml version="1.0" encoding="utf-8"?' : null,
+          Response: { Say: 'Touchdown, Bo Jackson!'}
+         }))
+ })
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {   
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
